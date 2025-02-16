a8a3b56be35f7c81c10b4d1ec43a60b0019983ad
Faizal Chan <moefc32@gmail.com>
2025-02-17 04:37:03 +0700

    add .prettierrc and fix indentations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,85 +1,85 @@
 <script>
-  import { onMount } from "svelte";
-  import { toast } from "svoast";
+    import { onMount } from 'svelte';
+    import { toast } from 'svoast';
 
-  import Title from "$lib/component/Title.svelte";
+    import Title from '$lib/component/Title.svelte';
 
-  // const globalSearchModalEl = document.querySelector("#global-search");
-  // const globalSearchModal =
-  //   globalSearchModalEl && new bootstrap.Modal(globalSearchModalEl, {});
+    // const globalSearchModalEl = document.querySelector("#global-search");
+    // const globalSearchModal =
+    //   globalSearchModalEl && new bootstrap.Modal(globalSearchModalEl, {});
 
-  let dataLoading = true;
-  let showcaseTab = 0;
-  let behance = [];
-  let dribbble = [];
-  let youtube = [];
+    let dataLoading = true;
+    let showcaseTab = 0;
+    let behance = [];
+    let dribbble = [];
+    let youtube = [];
 
-  // globalSearch: {
-  //   query: "",
-  //   queryModal: "",
-  //   toggle: false,
-  //   loading: false,
-  //   backend: "https://api.mf-chan.com/search",
-  //   searchTime: 0,
-  //   searchResult: [],
-  //   loadData: async function () {
-  //     searchResult = [];
+    // globalSearch: {
+    //   query: "",
+    //   queryModal: "",
+    //   toggle: false,
+    //   loading: false,
+    //   backend: "https://api.mf-chan.com/search",
+    //   searchTime: 0,
+    //   searchResult: [],
+    //   loadData: async function () {
+    //     searchResult = [];
 
-  //     try {
-  //       if (queryModal) {
-  //         loading = true;
-  //         const response = await fetch(
-  //           `${backend}?q=${queryModal}`
-  //         );
+    //     try {
+    //       if (queryModal) {
+    //         loading = true;
+    //         const response = await fetch(
+    //           `${backend}?q=${queryModal}`
+    //         );
 
-  //         searchTime = response.data.data.searchTime;
-  //         searchResult = response.data.data.searchResult;
-  //       }
-  //     } catch (e) {
-  //       console.error(e);
-  //     }
+    //         searchTime = response.data.data.searchTime;
+    //         searchResult = response.data.data.searchResult;
+    //       }
+    //     } catch (e) {
+    //       console.error(e);
+    //     }
 
-  //     loading = false;
-  //   },
-  //   open: async function () {
-  //     try {
-  //       if (query) {
-  //         queryModal = query;
-  //         query = "";
+    //     loading = false;
+    //   },
+    //   open: async function () {
+    //     try {
+    //       if (query) {
+    //         queryModal = query;
+    //         query = "";
 
-  //         globalSearchModal.show();
-  //         await loadData();
-  //       }
-  //     } catch (e) {
-  //       console.error(e);
-  //     }
-  //   },
-  // }
+    //         globalSearchModal.show();
+    //         await loadData();
+    //       }
+    //     } catch (e) {
+    //       console.error(e);
+    //     }
+    //   },
+    // }
 
-  onMount(async () => {
-    try {
-      const response = await fetch(import.meta.env.VITE_BACKEND);
-      const { data } = await response.json();
+    onMount(async () => {
+        try {
+            const response = await fetch(import.meta.env.VITE_BACKEND);
+            const { data } = await response.json();
 
-      behance = data.behance;
-      dribbble = data.dribbble;
-      youtube = data.youtube;
-      dataLoading = false;
-    } catch (e) {
-      console.error(e);
-      toast.error(
-        "Cannot fetch data from the backend, please try again later."
-      );
-    }
-  });
+            behance = data.behance;
+            dribbble = data.dribbble;
+            youtube = data.youtube;
+            dataLoading = false;
+        } catch (e) {
+            console.error(e);
+            toast.error(
+                'Cannot fetch data from the backend, please try again later.',
+            );
+        }
+    });
 </script>
 
 <Title />
 
 <main class="flex-1 mx-12 my-6">
-  <h1>Welcome to SvelteKit</h1>
-  <p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-  </p>
+    <h1>Welcome to SvelteKit</h1>
+    <p>
+        Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
+        the documentation
+    </p>
 </main>
