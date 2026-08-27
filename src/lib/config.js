133ff0a9b6fe580f5d694b61f3c4f4973c09269c
Faizal Chan. <moefc32@gmail.com>
2026-08-27 20:09:07 +0700

    finalizing the design of blog page

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,11 @@
 const pageSize = parseInt(import.meta.env.PUBLIC_PAGINATION_ITEMS || '10', 10);
 
+export const owner = {
+    displayName: 'Faizal Chan.',
+    introduction: 'I shape solutions by balancing usability, structure, and intent beyond surface-level visuals.',
+    shortTitle: 'UX Engineer & Researcher',
+};
+
 export const blog = {
     title: {
         long: 'Reasoning Processes',
