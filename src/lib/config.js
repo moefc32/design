85833397688a1cc036cddb21ffa4bf1045abbfe4
Faizal Chan. <moefc32@gmail.com>
2026-08-30 19:09:04 +0700

    improve Open Graph

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,6 +2,7 @@ const pageSize = parseInt(import.meta.env.PUBLIC_PAGINATION_ITEMS || '10', 10);
 
 export const owner = {
     displayName: 'Faizal Chan.',
+    displayPicture: '/res/favicon.png',
     introduction: 'I shape solutions by balancing usability, structure, and intent beyond surface-level visuals.',
     shortTitle: 'UX Engineer & Researcher',
 };
