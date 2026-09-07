3e4ee92a70fbd413d0e5596d15dd7d47e5b95ffc
Faizal Chan. <moefc32@gmail.com>
2026-09-08 01:55:19 +0700

    add /llms.txt

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,5 @@
+const siteName = `Faizal Chan's Website`;
+const title = `${import.meta.env.PUBLIC_APP_NAME} - ${siteName}`;
 const pageSize = parseInt(import.meta.env.PUBLIC_PAGINATION_ITEMS || '10', 10);
 
 export const owner = {
@@ -5,6 +7,13 @@ export const owner = {
     displayPicture: '/res/favicon.png',
     introduction: 'I shape solutions by balancing usability, structure, and intent beyond surface-level visuals.',
     shortTitle: 'UX Engineer & Researcher',
+};
+
+export const metadata = {
+    siteName,
+    title,
+    description: 'Discover design portfolios by Faizal Chan, highlighting visual design projects, videography, and creative solutions.',
+    type: 'website',
 };
 
 export const blog = {
@@ -15,3 +24,31 @@ export const blog = {
     path: '/reasoning-processes',
     pageSize,
 };
+
+export const navigations = [
+    {
+        name: 'Main Website',
+        link: 'https://mf-chan.com',
+    },
+    {
+        name: 'Research',
+        link: 'https://research.mf-chan.com',
+    },
+    {
+        name: 'Design',
+        link: 'https://design.mf-chan.com',
+    },
+    {
+        name: 'Code',
+        link: 'https://code.mf-chan.com',
+    },
+    {
+        name: 'Mentor',
+        link: 'https://mentor.mf-chan.com',
+    },
+    {
+        name: 'Certificate',
+        link: 'https://certificate.mf-chan.com',
+    },
+];
+
